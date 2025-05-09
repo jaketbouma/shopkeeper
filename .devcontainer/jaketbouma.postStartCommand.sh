c43ecabf7939d3d8ec3d83f19061667bb715aab4
Jake <jake.t.bouma@gmail.com>
2025-05-09 15:53:46 +0200

    Devcontainer and docs (#4)
    * oh boy devcontainer+vscode you gave me a hell of a run
    * some docs :)

--- .devcontainer/jaketbouma.postStartCommand.sh
+++ .devcontainer/jaketbouma.postStartCommand.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 # check that $GITHUB_USERNAME is not empty
 if [ -z "$GITHUB_USERNAME" ]; then
   echo "GITHUB_USERNAME is not set. Please set it to your GitHub username."
@@ -9,11 +11,11 @@ mkdir -p ~/.config/chezmoi
 # I have templated .zshrc
 cat <<EOF > ~/.config/chezmoi/chezmoi.toml
 [data]
-    histfile = "/workspace/.history"
+    histfile = "/workspaces/.zsh_history"
 EOF
   
 # Initialize chezmoi
 chezmoi init --apply $GITHUB_USERNAME
 
 # Initialize poetry
-cd /workspace/shopkeeper/pulumi-shopkeeper && poetry install
+cd /workspaces/shopkeeper/pulumi-shopkeeper && poetry install
