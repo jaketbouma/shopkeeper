d222441ceb18a69eed832f6bdf1e168d3fded83b
Jake Bouma <jake.t.bouma@gmail.com>
2025-05-28 13:25:41 +0000

    code generation for componentresources

--- pulumi-shopkeeper/shopkeeper/backend_factory.py
+++ pulumi-shopkeeper/shopkeeper/backend_factory.py
@@ -1,12 +1,62 @@
-from shopkeeper.aws_market import AWSMarketBackend  # noqa F401
-from shopkeeper.backend_interface import MarketBackend  # noqa F401
+import os
 from typing import Type
 
+from jinja2 import Environment, FileSystemLoader
+from pulumi import ComponentResource
 
-MARKET_BACKENDS = {"aws:v1": AWSMarketBackend, "aws:latest": AWSMarketBackend}
+from shopkeeper.aws_market import AWSBackendDeclaration, AWSMarketBackend  # noqa F401
+from shopkeeper.backend_interface import MarketBackend  # noqa F401
+from shopkeeper.generated import components
+
+MARKET_BACKENDS = {
+    "AWSMarketBackendV1": {
+        "component_type": AWSMarketBackend,
+        "args_type": AWSBackendDeclaration,
+    },
+    "AWSMarketBackendLatest": {
+        "component_type": AWSMarketBackend,
+        "args_type": AWSBackendDeclaration,
+    },
+}
 
 
 def get(backend_type) -> Type[MarketBackend]:
     if backend_type not in MARKET_BACKENDS:
         raise KeyError("Unknown Backend")
-    return MARKET_BACKENDS[backend_type]
+    return MARKET_BACKENDS[backend_type]["component_type"]
+
+
+def get_components() -> list[ComponentResource]:
+    return components.components
+
+
+def __generate_components():
+    """
+    Renders the component.py.jinja template with example data and writes to ./generated/components.py
+    """
+    env = Environment(
+        loader=FileSystemLoader(os.path.dirname(__file__)),
+        trim_blocks=True,
+        lstrip_blocks=True,
+    )
+    template = env.get_template("components.py.jinja")
+    example_data = {
+        "components": [
+            {
+                "component_module": v["component_type"].__module__,
+                "args_type_name": v["args_type"].__name__,
+                "component_name": k,
+            }
+            for k, v in MARKET_BACKENDS.items()
+        ]
+    }
+    rendered = template.render(**example_data)
+    project_root = os.path.dirname(os.path.dirname(__file__))
+    output_dir = os.path.join(project_root, "shopkeeper/generated")
+    os.makedirs(output_dir, exist_ok=True)
+    with open(os.path.join(output_dir, "components.py"), "w") as f:
+        f.write(rendered)
+
+
+if __name__ == "__main__":
+    __generate_components()
--- pulumi-shopkeeper/shopkeeper/component_factory.py
+++ pulumi-shopkeeper/shopkeeper/component_factory.py
@@ -1,11 +1,7 @@
-from typing import Any, Optional, Type
+from typing import Optional, Type
 
-from pulumi import Output, ResourceOptions
-from pulumi.provider.experimental import component_provider_host
+from pulumi import ResourceOptions
 
-from shopkeeper.aws_market import (
-    AWSBackendDeclaration,
-)
 from shopkeeper.market import ComponentResource
 
 
@@ -13,29 +9,9 @@ def ComponentClassFactory(
     component_name: str,
     Args,
 ) -> Type[ComponentResource]:
-    class GeneratedComponent(ComponentResource):
-        market_data: Output[Any]
-
-        def __init__(
-            self,
-            name: str,
-            args: Args,  # type: ignore
-            opts: Optional[ResourceOptions] = None,
-        ):
-            super().__init__(
-                f"pulumi-shopkeeper:index:{component_name}", name, props={}, opts=opts
-            )
-            self.market_data = {"berry": "blue"}
-            self.register_outputs({"marketData": self.market_data})
-
-    GeneratedComponent.__name__ = component_name
-    return GeneratedComponent
-
-
-def ComponentClassFactory2(
-    component_name: str,
-    Args,
-) -> Type[ComponentResource]:
+    """
+    Abandoned -- dynamic types with Pulumi is asking for trouble.
+    """
     marketData: dict[str, str] = {"banana": "yellow"}
 
     def _constructor(
@@ -58,17 +34,3 @@ def ComponentClassFactory2(
         {"__init__": _constructor, "marketData": marketData},
     )
     return T
-
-
-if __name__ == "__main__":
-    components = [
-        ComponentClassFactory2(component_name="AWSMarket", Args=AWSBackendDeclaration),
-        ComponentClassFactory2(
-            component_name="AWSMarket2",
-            Args=AWSBackendDeclaration,
-        ),
-    ]
-    component_provider_host(
-        name="pulumi-shopkeeper",
-        components=components,
-    )
